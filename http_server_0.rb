99073ef7553de702f1e966143058448f44f9ebc2
John Mettraux <jmettraux@gmail.com>
2018-03-22 17:55:53 +0900

    Implement http server 0

---
+++ http_server_0.rb
@@ -0,0 +1,19 @@
+
+require 'socket'
+
+server = TCPServer.new(8080)
+
+loop do
+
+  client = server.accept
+
+  s = ''
+  while ! s.match(/\r\n\r\n\z/)
+    s += client.read(1)
+  end
+  puts s
+
+  client.puts s
+  client.close
+end
+
